ee747ea5adee0ba1fb4d5ec065b194a2428dbc92
mathew <69637595+wa2f@users.noreply.github.com>
2023-11-22 09:09:58 +0300

    initial backend setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ trans('Strings.name') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+    
+</head>
+
+<body>
+    {{-- @if (Route::has('login'))
+        @auth
+            <a href="{{ url('/home') }}"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+        @else
+            <a href="{{ route('login') }}"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+                in
+            </a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+            @endif
+        @endauth
+    @endif --}}
+    <p>{{ trans('Strings.welcome_txt') }}</p>
+
+</body>
+
+</html>
