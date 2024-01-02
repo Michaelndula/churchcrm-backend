cf594ecafff869026f3d5d8be323cd13f6f81c56
Njeri <85888329+Njeri-Ngugi@users.noreply.github.com>
2024-01-02 15:42:04 +0300

    Edited login and profile routes

--- app/Http/Controllers/MobileApiController.php
+++ app/Http/Controllers/MobileApiController.php
@@ -8,27 +8,56 @@ use App\Models\SermonNotes;
 use App\Models\Sermons;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class MobileApiController extends Controller
 {
     public function fetchEvents()
     {
-        $data = Event::all(); 
+        $data = Event::all();
         return response()->json($data);
     }
     public function fetchAnnouncements()
     {
-        $data = Announcement::all(); 
+        $data = Announcement::all();
         return response()->json($data);
     }
     public function fetchSermonnotes()
     {
-        $data = SermonNotes::all(); 
+        $data = SermonNotes::all();
         return response()->json($data);
     }
     public function fetchSermons()
     {
-        $data = Sermons::all(); 
+        $data = Sermons::all();
         return response()->json($data);
+    }
+    public function fetchProfile($id)
+    {
+        $data = User::where('id', $id)->first();
+        return response()->json($data);
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $token = $request->user()->createToken('authToken')->plainTextToken;
+
+            $user = Auth::id();
+
+            return response()->json(
+                [
+                    'token' => $token,
+                    'email' => $credentials['email'],
+                    'userId' => $user,
+                ]
+            );
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
