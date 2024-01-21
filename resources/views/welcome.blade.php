4b29922d31f61a37440307f8dbaf0d644e55463c
mathewwahome <mathewwahome2003@gmail.com>
2024-01-21 14:43:09 +0300

    modified user profile to get $userid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,48 +13,12 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="assets/css/styles.css">
-    
+
 </head>
 
 <body>
-    {{-- @if (Route::has('login'))
-        @auth
-            <a href="{{ url('/dashboard') }}"
-                class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-        @else
-            <a href="{{ route('login') }}"
-                class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                in
-            </a>
-
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}"
-                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-            @endif
-        @endauth
-    @endif --}}
     <p>{{ trans('Strings.welcome_txt') }}</p>
-    <div id="app">
-        <h1>User Data api test</h1>
-        <ul id="user-list"></ul>
-    </div>
-
-    <script>
-        // Fetch data from the API
-        fetch('http://127.0.0.1:8000/api/fetch-data')
-            .then(response => response.json())
-            .then(data => {
-                // Display data in the list
-                const userList = document.getElementById('user-list');
-                data.forEach(user => {
-                    const listItem = document.createElement('li');
-                    listItem.textContent = `${user.name} - ${user.email}`;
-                    userList.appendChild(listItem);
-                });
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    </script>
-
+    <p>ChurchCRM</p>
 </body>
 
 </html>
