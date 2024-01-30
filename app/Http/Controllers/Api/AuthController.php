c8539afff1c52c89bee98fe2d489eda9dce18804
Njeri <85888329+Njeri-Ngugi@users.noreply.github.com>
2024-01-30 12:33:57 +0300

    Update profile function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -41,6 +41,56 @@ class AuthController extends Controller
             return response()->json(['message' => 'Password do not match']);
         }
     }
+    // Update User
+    public function update_user(Request $request, $id) {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'phone' => 'required|string|max:12|min:10',
+            'email' => 'required|email|unique:app_users,email',
+            'password' => 'required|string|min:8',
+            'profile' => 'image|mimes:jpeg,png,jpg,svg|max:2048',
+            'membership_status' => 'string',
+        ]);
+
+        $profile_pic = $validated['profile'];
+
+        $user = AppUser::findOrFail($id);
+        if($user) {
+            $user->name = $validated['name'];
+            $user->email = $validated['email'];
+            $user->phone = $validated['phone'];
+            $user->password = Hash::make($validated['password']);
+            $user->membership_status = $validated['membership_status'];
+
+            //Saving the profile photo
+            // if ($profile_pic) {
+            //     $validExtensions = ['jpeg', 'png', 'jpg', 'webp', 'svg'];
+            //     $fileExtension = strtolower($profile_pic->getClientOriginalExtension());
+
+            //     if (!in_array($fileExtension, $validExtensions)) {
+            //         return redirect()
+            //             ->back()
+            //             ->with('error', 'Invalid file format. Please upload a jpeg, jpg,  png, webp, svg file.');
+            //     }
+            //     $profile_pic_path = time() . '.' . $fileExtension;
+            //     $profile_pic->move('Mobile_App_Profile_Pics/', $profile_pic_path);
+            // }
+
+            // $user->profile_photo_path = $profile_pic_path;
+            $user->profile_photo_path = 'default_user_profile.jpeg';
+
+            $user->save();
+        }
+        else {
+            return response()->json(['message' => 'Error! User does not exist']);
+        }
+     }
+
+    //  Delete user account 
+    public function delete_user($id) {
+        
+    }
+
     public function login(Request $request)
     {
         $credentials = $request->validate([
