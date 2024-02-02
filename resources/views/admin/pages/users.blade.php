1362dcca2540df5f21442f580ee5a34bfee9a369
mathewwahome <methewwahome@gmail.com>
2024-02-02 14:46:33 +0300

    users tab modification

--- resources/views/admin/pages/users.blade.php
+++ resources/views/admin/pages/users.blade.php
@@ -2,21 +2,8 @@
 <html lang="en">
 
 <head>
-    {{-- @include('admin.layout.head') --}}
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Church CRM</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    {{-- icons --}}
-    <script src="https://kit.fontawesome.com/30ef7ac8b3.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-
-    {{-- Start New css --}}
-    <link rel="stylesheet" href="assets/css/newcss.css">
-
-    {{-- End New css --}}
+    @include('admin.layout.head')
+   
 
 </head>
 
