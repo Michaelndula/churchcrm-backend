cdfd9ffd836b1ae1df9afbe5cb799b486efefbce
mathewwahome <methewwahome@gmail.com>
2024-02-15 12:20:37 +0300

    completion of the code reset password

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use App\Models\AppUser;
-use App\Models\Password_reset_code;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
 
 class ResetPasswordController extends Controller
@@ -55,42 +55,50 @@ class ResetPasswordController extends Controller
     public function resetcode(Request $request)
     {
         try {
-            $id = 1;
             $code = $request->code;
 
-            $user = AppUser::where('password_reset_code', '=', $code)->get();
+            $user = AppUser::where('password_reset_code', '=', $code)->first();
+
             if ($user) {
-                return response()->json(['message' => $code], 200);
-                // TODO - solve the diffminute error
-                // if ($code && $user->password_reset_code->diffInMinutes(now()) < 60) {
-                //     return response()->json(['message' => 'reset code available'], 200);
-                // } else {
-                //     return false;
-                // }
+                // Check if the password reset code was generated within the last 60 minutes
+                if ($code && Carbon::parse($user->password_reset_code_time)->diffInMinutes(now()) < 60) {
+                    return response()->json(['message' => 'reset code available'], 200);
+                } else {
+                    return response()->json(['message' => 'reset code expired'], 400);
+                }
+                
+            } else {
+                return response()->json(['error' => 'Invalid reset code'], 404);
             }
         } catch (\Throwable $th) {
             throw $th;
         }
     }
+
     public function resetpassword(Request $request, $code)
     {
         $credentials = $request->validate([
             'newpassword' => 'required|string',
             'confirmpassword' => 'required|string',
         ]);
-        $user = AppUser::where('password_reset_code', $code)->get();
+
+        $user = AppUser::where('password_reset_code', $code)->first();
+
         if ($user) {
             $newpassword = $credentials['newpassword'];
             $confirmpassword = $credentials['confirmpassword'];
-            if ($newpassword == $confirmpassword) {
-                $password = Hash::make($credentials['confirmpassword']);
-                $user->password = $password;
 
-                // $user->update([
-                //     'password' => $password,
-                // ]);
-                $user->save();
-                return response()->json(['newpassword' => $newpassword], 200);
+            if ($newpassword === $confirmpassword) {
+                $hashedPassword = Hash::make($newpassword);
+                $user->password = $hashedPassword;
+                $passchange = $user->save();
+                if ($passchange) {
+                    //send an email
+
+                    return response()->json(['message' => 'Password reset successfully'], 200);
+                }
+            } else {
+                return response()->json(['error' => 'Passwords do not match'], 400);
             }
         } else {
             return response()->json(['error' => 'User not found'], 404);
